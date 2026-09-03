f05defa405c9e22657c4a5c03120eb36acd50c34
lucero <60794255@institutourusayhua.edu.pe>
2026-09-03 11:44:44 -0500

    se agrego nombre del curso al codigo

---
+++ registro_estudiante.py
@@ -0,0 +1,9 @@
+print("herramientas de desarrollo de softwar")
+print("REGISTRO DE ESTUDIANTE")
+
+nombre = input("ingrese su nombre: ")
+edad = int(input("ingrese su edad: "))
+
+print("/nDATOS REGISTRADOS")
+print(" nombre: ", nombre)
+print("edad: ", edad)
